9581d15734923ea2b8002a3d3f8dfd5a34074c13
npnelson <nnelson1@insight.rr.com>
2020-06-02 11:08:41 -0400

    Added Site and Slot name for Azure Functions to Properties

--- src/NetToolBox.SerilogEnrichers/AspNetEnvironmentEnricher.cs
+++ src/NetToolBox.SerilogEnrichers/AspNetEnvironmentEnricher.cs
@@ -7,32 +7,82 @@ namespace NetToolBox.SerilogEnrichers
 {
     public sealed class AspNetEnvironmentEnricher : ILogEventEnricher
     {
-        LogEventProperty _cachedProperty = null!;
-
-        public const string PropertyName = "Environment";
+        LogEventProperty _cachedEnvironment = null!;
+        LogEventProperty _cachedSiteName = null!;
+        LogEventProperty _cachedSlotName = null!;
+        public const string SiteName = "SiteName";
+        public const string SlotName = "SlotName";
+        public const string EnvironmentName = "Environment";
 
         public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
         {
-            logEvent.AddPropertyIfAbsent(GetLogEventProperty(propertyFactory));
+            logEvent.AddPropertyIfAbsent(GetLogEventProperty(propertyFactory, EnvironmentName));
+            logEvent.AddPropertyIfAbsent(GetLogEventProperty(propertyFactory, SiteName));
+            logEvent.AddPropertyIfAbsent(GetLogEventProperty(propertyFactory, SlotName));
         }
-        private LogEventProperty GetLogEventProperty(ILogEventPropertyFactory propertyFactory)
+        private LogEventProperty GetLogEventProperty(ILogEventPropertyFactory propertyFactory, string propertyName)
         {
-            if (_cachedProperty == null)
+            if (propertyName == EnvironmentName)
             {
-                _cachedProperty = CreateProperty(propertyFactory);
+                if (_cachedEnvironment == null)
+                {
+                    _cachedEnvironment = CreateEnvironmentProperty(propertyFactory);
+                }
+                return _cachedEnvironment;
             }
-            return _cachedProperty;
+            else if (propertyName == SiteName)
+            {
+                if (_cachedSiteName == null)
+                {
+                    _cachedSiteName = CreateSiteNameProperty(propertyFactory);
+                }
+                return _cachedSiteName;
+            }
+            else if (propertyName == SlotName)
+            {
+                if (_cachedSlotName == null)
+                {
+                    _cachedSlotName = CreateSlotNameProperty(propertyFactory);
+                }
+                return _cachedSlotName;
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid PropertyName");
+            }
         }
 
         [MethodImpl(MethodImplOptions.NoInlining)]
-        private static LogEventProperty CreateProperty(ILogEventPropertyFactory propertyFactory)
+        private static LogEventProperty CreateSlotNameProperty(ILogEventPropertyFactory propertyFactory)
+        {
+            var prop = Environment.GetEnvironmentVariable("APPSETTING_WEBSITE_SLOT_NAME");
+            if (prop == null)
+            {
+                prop = "Production";
+            }
+            return propertyFactory.CreateProperty(SlotName, prop);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static LogEventProperty CreateSiteNameProperty(ILogEventPropertyFactory propertyFactory)
+        {
+            var prop = Environment.GetEnvironmentVariable("APPSETTING_WEBSITE_SITE_NAME");
+            if (prop == null)
+            {
+                prop = "Not Set";
+            }
+            return propertyFactory.CreateProperty(SiteName, prop);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static LogEventProperty CreateEnvironmentProperty(ILogEventPropertyFactory propertyFactory)
         {
             var prop = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
             if (prop == null)
             {
                 prop = "Production";
             }
-            return propertyFactory.CreateProperty(PropertyName, prop);
+            return propertyFactory.CreateProperty(EnvironmentName, prop);
         }
     }
 }
