0b3041c03e12e3511f67544206beac750698ebfe
Nick Nelson <NicholasPNelson@hotmail.com>
2021-02-17 12:19:18 -0500

    try to fix azure and ef parameters for logging

--- src/NetToolBox.SerilogEnrichers/ServiceCollectionExtensions.cs
+++ src/NetToolBox.SerilogEnrichers/ServiceCollectionExtensions.cs
@@ -8,25 +8,27 @@ namespace Microsoft.Extensions.DependencyInjection
         public static IServiceCollection AddSeqWithCommonEnrichers<T>(this IServiceCollection serviceCollection, string seqUrl, string apiKey, string environmentName, bool includeEFInformationLogging, bool includeAzureLogging)
         {
             var loggerConfiguration = new LoggerConfiguration()
-                 .MinimumLevel.Debug()
-                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning);
+               .MinimumLevel.Debug()
+               .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+               .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning);
+
             if (!includeEFInformationLogging)
             {
-                loggerConfiguration = loggerConfiguration.MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning);
+                loggerConfiguration.MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning);
             }
             if (!includeAzureLogging)
             {
-                loggerConfiguration = loggerConfiguration.MinimumLevel.Override("Azure-", LogEventLevel.Warning); //the Azure-Identity,Azure-Core, and Azure-Messaging-ServiceBus sourcecontexts can be quite noisy and rarely useful
+                loggerConfiguration.MinimumLevel.Override("Azure-Core", LogEventLevel.Warning) //the Azure-Identity,Azure-Core, and Azure-Messaging-ServiceBus sourcecontexts can be quite noisy and rarely useful
+            .MinimumLevel.Override("Azure-Identity", LogEventLevel.Warning);
             }
-            loggerConfiguration = loggerConfiguration.Enrich.WithClrVersion()
-            .Enrich.WithOSVersion()
-            .Enrich.WithProperty("Environment", environmentName)
-            .Enrich.WithApplicationVersion<T>()
-            .Enrich.WithAzureFunctionHostVersion()
-            .Enrich.WithMachineName()
-            .WriteTo.Seq(seqUrl, apiKey: apiKey)
-            .WriteTo.Console();
+            loggerConfiguration.Enrich.WithClrVersion()
+        .Enrich.WithOSVersion()
+        .Enrich.WithProperty("Environment", environmentName)
+        .Enrich.WithApplicationVersion<T>()
+        .Enrich.WithAzureFunctionHostVersion()
+        .Enrich.WithMachineName()
+        .WriteTo.Seq(seqUrl, apiKey: apiKey)
+        .WriteTo.Console();
 
             var logger = loggerConfiguration.CreateLogger();
             serviceCollection.AddLogging(lb => lb.AddSerilog(logger));
