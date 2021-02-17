4bdb508675654b65d34fb419cf6287c1582c26ac
Nick Nelson <NicholasPNelson@hotmail.com>
2021-02-17 10:19:26 -0500

    add parameters to tweak ef and azure logging

--- src/NetToolBox.SerilogEnrichers/ServiceCollectionExtensions.cs
+++ src/NetToolBox.SerilogEnrichers/ServiceCollectionExtensions.cs
@@ -1,21 +1,34 @@
 ﻿using Serilog;
+using Serilog.Events;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddSeqWithCommonEnrichers<T>(this IServiceCollection serviceCollection, string seqUrl, string apiKey, string environmentName)
+        public static IServiceCollection AddSeqWithCommonEnrichers<T>(this IServiceCollection serviceCollection, string seqUrl, string apiKey, string environmentName, bool includeEFInformationLogging, bool includeAzureLogging)
         {
-            var logger = new LoggerConfiguration()
-               .Enrich.WithClrVersion()
-               .Enrich.WithOSVersion()
-               .Enrich.WithProperty("Environment", environmentName)
-               .Enrich.WithApplicationVersion<T>()
-               .Enrich.WithAzureFunctionHostVersion()
-               .Enrich.WithMachineName()
-               .WriteTo.Seq(seqUrl, apiKey: apiKey)
-               .WriteTo.Console()
-               .CreateLogger();
+            var loggerConfiguration = new LoggerConfiguration()
+                 .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning);
+            if (!includeEFInformationLogging)
+            {
+                loggerConfiguration.MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning);
+            }
+            if (!includeAzureLogging)
+            {
+                loggerConfiguration.MinimumLevel.Override("Azure-", LogEventLevel.Warning); //the Azure-Identity,Azure-Core, and Azure-Messaging-ServiceBus sourcecontexts can be quite noisy and rarely useful
+            }
+            loggerConfiguration.Enrich.WithClrVersion()
+            .Enrich.WithOSVersion()
+            .Enrich.WithProperty("Environment", environmentName)
+            .Enrich.WithApplicationVersion<T>()
+            .Enrich.WithAzureFunctionHostVersion()
+            .Enrich.WithMachineName()
+            .WriteTo.Seq(seqUrl, apiKey: apiKey)
+            .WriteTo.Console();
+
+            var logger = loggerConfiguration.CreateLogger();
             serviceCollection.AddLogging(lb => lb.AddSerilog(logger));
             return serviceCollection;
         }
